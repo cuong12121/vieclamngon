f094fe290fecd7245a2ac1d0d2b9a309e5e2b297
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-03-20 16:24:01 +0700

    Update myprofile.blade.php

--- resources/views/user/myprofile.blade.php
+++ resources/views/user/myprofile.blade.php
@@ -1889,6 +1889,27 @@
                 </div>
             </div>
             <script>
+
+                function choose_file() {
+                    var file_data = $('#fileAvatar').prop('files')[0];   
+                    var form_data = new FormData();                  
+                    form_data.append('file', file_data);
+                    alert(form_data);                             
+                    // $.ajax({
+                    //     url: 'upload.php', // <-- point to server-side PHP script 
+                    //     dataType: 'text',  // <-- what to expect back from the PHP script, if anything
+                    //     cache: false,
+                    //     contentType: false,
+                    //     processData: false,
+                    //     data: form_data,                         
+                    //     type: 'post',
+                    //     success: function(php_script_response){
+                    //         alert(php_script_response); // <-- display response from the PHP script, if any
+                    //     }
+                    //  });
+                }
+
+                
                 function openTipSlide(idTip) {
 
 
@@ -1915,26 +1936,7 @@
                 }
 
 
-                function choose_file() {
-
-                    console.log(1);
-                    // var file_data = $('#fileAvatar').prop('files')[0];   
-                    // var form_data = new FormData();                  
-                    // form_data.append('file', file_data);
-                    // alert(form_data);                             
-                    // $.ajax({
-                    //     url: 'upload.php', // <-- point to server-side PHP script 
-                    //     dataType: 'text',  // <-- what to expect back from the PHP script, if anything
-                    //     cache: false,
-                    //     contentType: false,
-                    //     processData: false,
-                    //     data: form_data,                         
-                    //     type: 'post',
-                    //     success: function(php_script_response){
-                    //         alert(php_script_response); // <-- display response from the PHP script, if any
-                    //     }
-                    //  });
-                }
+                
             </script>
         </div>
     </div>
