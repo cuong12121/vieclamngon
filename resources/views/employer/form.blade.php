dee0c6a41b84353e910f8c792843a49a231a039b
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-03-20 15:50:52 +0700

    Update form.blade.php

--- resources/views/employer/form.blade.php
+++ resources/views/employer/form.blade.php
@@ -115,10 +115,7 @@
 
                 @if(Auth::guard('employer_register')->check())
 
-                <?php 
-
-                    dd(Auth::guard('employer_register'));
-                ?>
+                
                 <div class="main-login dropdown logged"><a href="{{ route('index_employer') }}" > Hi, <span class="name">{{ @Auth::guard('employer_register')->user()->name }}</span></a>
                     <div class="dropdown-menu">
                         <ul>
