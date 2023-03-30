b5a2c1e874b426db46ff7c7f50de53fa25103c2e
Cuongdao2222 <cuongdao12121@gmail.com>
2023-03-30 14:52:29 +0700

    Update filterController.php

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -35,7 +35,7 @@ class filterController extends Controller
 
         $industry = $request->industry??'';
 
-        $job = DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')->Where('job.title','LIKE', '%'. $datas .'%')->Where('job.address_job','LIKE', '%'.$address.'%')->Where('career','LIKE', '%'.$industry.'%')->Orwhere('employ_info.name','LIKE', '%'. $datas .'%')->Where('job.address_job','LIKE', '%'.$address.'%')->Where('career','LIKE', '%'.$industry.'%')->Orwhere('job.address_job', $address)->Orwhere('job.career', $industry)->get();
+        $job = DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')->Where('job.title','LIKE', '%'. $datas .'%')->Where('job.address_job', $address)->Where('career','LIKE', '%'.$industry.'%')->Orwhere('employ_info.name','LIKE', '%'. $datas .'%')->Where('job.address_job', $address)->Where('career','LIKE', '%'.$industry.'%')->Orwhere('job.address_job', $address)->Orwhere('job.career', $industry)->get();
 
         return view('frontend.filter', compact('job'));
     }
