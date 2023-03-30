9e377fc8eedcd8f81c6a7888bee3404dd76b80d8
Cuongdao2222 <cuongdao12121@gmail.com>
2023-03-30 15:03:40 +0700

    Update filterController.php

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -38,12 +38,10 @@ class filterController extends Controller
 
         if(empty($datas) && empty($address) && empty($industry)){
 
-
             $job = job::paginate(10);
             return view('frontend.filter', compact('job'));
         }
 
-        
         $job = DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')
         ->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')
         ->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')
@@ -53,7 +51,7 @@ class filterController extends Controller
         ->Orwhere('employ_info.name','LIKE', '%'. $datas .'%')
         ->Where('job.address_job', $address)
         ->Where('career','LIKE', '%'.$industry.'%')
-        ->Orwhere('job.address_job', $address)->Orwhere('job.career', $industry)->get();
+        ->Orwhere('job.address_job', $address)->where('job.career', $industry)->get();
 
         return view('frontend.filter', compact('job'));
     }
