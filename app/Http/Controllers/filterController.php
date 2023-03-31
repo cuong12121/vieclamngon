6f80a721b4822c85bd745b6c3f7a747664339db1
Cuongdao2222 <cuongdao12121@gmail.com>
2023-03-31 11:12:27 +0700

    Update filterController.php

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -52,15 +52,27 @@ class filterController extends Controller
         ->Where('job.address_job', $address)
         ->Where('career',$industry)
         ->Orwhere('job.address_job', $address)->where('job.career', $industry)->get();
-        if(empty($job) || $job->count()===0){
 
-            $job =   DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')
-            ->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')
-            ->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')
-            ->Where('job.address_job', $address)->Orwhere('job.career', $industry)->get();
+        if($job->count()===0){
+
+            // Trường hợp không nhập address va industry
+            if(empty($address)&& empty($industry)){
+
+                $job = DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')
+                ->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')
+                ->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')
+                ->Where('job.title','LIKE', '%'. $datas .'%')->get();
+            }
+
+            // Trường hợp không nhập keyword va industry
+            else(empty($keyword)&& empty($industry) || empty($keyword)&& empty($address)){
+                 $job =   DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')
+                ->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')
+                ->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')
+                ->Where('job.address_job', $address)->Orwhere('job.career', $industry)->get();
+            }
 
         }
-       
 
         return view('frontend.filter', compact('job'));
     }
