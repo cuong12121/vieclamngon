e40e86053596ea0d319df052a935f20f1993ed8d
Cuongdao2222 <cuongdao12121@gmail.com>
2023-03-31 13:54:36 +0700

    Update filterController.php

--- app/Http/Controllers/filterController.php
+++ app/Http/Controllers/filterController.php
@@ -62,10 +62,15 @@ class filterController extends Controller
                 ->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')
                 ->Where('job.title','LIKE', '%'. $datas .'%')->get();
             }
+            else{
+                
+                if(empty($datas)&& empty($industry) || empty($datas)&& empty($address)){
 
-            // Trường hợp không nhập keyword va industry
-            else(empty($datas)&& empty($industry) || empty($datas)&& empty($address)){
-                 $job =   DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')->where('job.address_job', $address)->Orwhere('job.career', $industry)->get();
+                    $job =   DB::table('employer_registers')->join('job', 'employer_registers.id', '=', 'job.employer_id')->join('employ_info', 'employer_registers.id', '=', 'employ_info.employ_id')->select('job.title', 'job.id', 'employ_info.name', 'employ_info.logo', 'employ_info.links', 'job.link', 'job.salary', 'job.address_job')->where('job.address_job', $address)->Orwhere('job.career', $industry)->get();
+
+                }
+
+
             }
 
         }
