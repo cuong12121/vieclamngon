3d3c64f1c6a79c7ae8eebda79771821aad78c299
Cuongdao2222 <cuongdao12121@gmail.com>
2023-04-14 10:48:10 +0700

    Update appfe.blade.php

--- resources/views/frontend/layout/appfe.blade.php
+++ resources/views/frontend/layout/appfe.blade.php
@@ -258,7 +258,7 @@
                     <div class="left-wrap">
                         <div class="button-hambuger"><span class="mdi mdi-menu"></span></div>
 
-                        @if(\Request::route()->getName()=='login-users'||\Request::route()->getName()=='registerClientUser'||\Request::route()->getName()=='job_details'||\Request::route()->getName()=='all_job'||\Request::route()->getName()=='filters'||\Request::route()->getName()=='update-password-user')
+                        @if(\Request::route()->getName()=='login-users'||\Request::route()->getName()=='registerClientUser'||\Request::route()->getName()=='job_details'||\Request::route()->getName()=='all_job'||\Request::route()->getName()=='filters'||\Request::route()->getName()=='update-password-user'||\Request::route()->getName()=='update-password-user-id')
                         <div class="logo"><a href="{{ route('home') }}  " title="Tuyển dụng & Tìm kiếm việc làm nhanh" ><img src="{{ asset('images/template/logo/logo.png') }}" alt="Tuyển dụng & Tìm kiếm việc làm nhanh"></a></div>
 
                         @else
