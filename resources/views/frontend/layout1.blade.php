311659e5121b70bea76df3c9cea1581819e13b41
Cuongdao2222 <cuongdao12121@gmail.com>
2023-08-24 16:07:20 +0700

    Update layout1.blade.php

--- resources/views/frontend/layout1.blade.php
+++ resources/views/frontend/layout1.blade.php
@@ -22,12 +22,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
       <!--   <link rel="shortcut icon" href="https://www.chotot.com/chotot-img/favicon.ico"/>
         <link rel="shortcut icon" href="https://static.chotot.com/storage/chotot-icons/favicon.ico"/> -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WCFN73J');
-        </script>
+       
         <link rel="canonical" href="//>
         <script async="" src="https://static.chotot.com/storage/js/prebid-ads.js"></script>
         <meta name="theme-color" content="#2561CF"/>
